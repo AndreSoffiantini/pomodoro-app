f4f90e1333c28522e8f19bfe47ad8dbae8bbca81
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-11-08 17:41:35 +0100

    Add logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.css";
+import pomodoroLogo from "./img/pomodoro-logo.png";
 import TimerSection from "./components/TimerSection";
 
 function App() {
-  return <TimerSection />;
+  return (
+    <>
+      <div className="main-title">
+        <img className="pomodoro-logo" src={pomodoroLogo} alt="pomodoro-logo" />
+        <h1 className="pomodoro-title">Pomodoro App</h1>
+      </div>
+
+      <TimerSection />
+    </>
+  );
 }
 
 export default App;
